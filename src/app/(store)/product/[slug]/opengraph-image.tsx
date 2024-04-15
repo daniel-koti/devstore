38df2b80a6a52d5fc716197448c2f29fbcfb89b4
Daniel Moniz <danielkoti.moniz@gmail.com>
2024-04-15 01:00:06 +0100

    seo: config opengraph-image

---
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,59 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import { ImageResponse } from 'next/og'
+import { env } from '@/env'
+import colors from 'tailwindcss/colors'
+
+// Route segment config
+export const runtime = 'edge'
+
+// Image metadata
+export const alt = 'About Acme'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+
+  const product = await response.json()
+  return product
+}
+
+// Image generation
+export default async function OgImage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await getProduct(params.slug)
+
+  const productImageURL = new URL(product.image, env.APP_URL).toString()
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          background: colors.zinc[950],
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <img src={productImageURL} alt="" style={{ widows: '100%' }} />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  )
+}
